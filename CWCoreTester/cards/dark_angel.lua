eea5b44dd66762ffb19469171b9416b8b51a2bda
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-08 04:15:44 -0400

    Add Dark Angel

--- CWCoreTester/cards/dark_angel.lua
+++ CWCoreTester/cards/dark_angel.lua
@@ -4,13 +4,13 @@ function _Create(props)
     local result = CardWars:Creature(props)
 
     result:AddStateModifier(function (state, me)
-        -- +2 DEF for every 5 cards in your discard pile.
+        -- +1 ATK for every 5 cards in your discard pile.
 
         local ownerI = me.Original.OwnerI
         local player = state.Players[ownerI]
         local discardCount = player.DiscardPile.Count
 
-        me.Defense = me.Defense + math.floor(discardCount / 5) * 2
+        me.Attack = me.Attack + math.floor(discardCount / 5)
     end)
 
     return result
