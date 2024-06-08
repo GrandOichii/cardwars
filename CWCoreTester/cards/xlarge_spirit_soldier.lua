338378292bd20e640d0803b53bd4ea2adaf50f6c
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-08 04:27:23 -0400

    Add X_Large Spirit Soldier

---
+++ CWCoreTester/cards/xlarge_spirit_soldier.lua
@@ -0,0 +1,21 @@
+-- Status: Implemented
+
+function _Create(props)
+    local result = CardWars:Creature(props)
+
+    result:AddStateModifier(function (state, me)
+        -- Each adjacent Creature has +1 ATK.
+
+        local ownerI = me.Original.OwnerI
+        local id = me.Original.Card.ID
+
+        local adjacent = Common.State:AdjacentLandscapes(state, ownerI, me.LaneI)
+        for _, landscape in ipairs(adjacent) do
+            if landscape.Creature ~= nil then
+                landscape.Creature.Attack = landscape.Creature.Attack + 1
+            end
+        end
+    end)
+
+    return result
+end
