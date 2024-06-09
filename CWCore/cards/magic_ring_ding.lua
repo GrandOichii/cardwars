53afef706e4d4a12469e9ca928395239638f8996
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-09 04:07:29 -0400

    Add Magic Ring Ding

---
+++ CWCore/cards/magic_ring_ding.lua
@@ -0,0 +1,25 @@
+-- Status: Implemented, not tested
+
+function _Create(props)
+    local result = CardWars:Spell(props)
+    
+    result.EffectP:AddLayer(
+        function (playerI)
+            -- Each of your Creatures has +1 ATK this turn for every 5 cards in your discard pile.
+
+            UntilEndOfTurn(function (state)
+                local creatures = Common.State:FilterCreatures(state, function (creature)
+                    return creature.Original.OwnerI == playerI
+                end)
+
+                local amount = state.Players[playerI].DiscardPile.Count
+                for _, creature in ipairs(creatures) do
+                    creature.Attack = creature.Attack + math.floor(amount / 5)
+                end
+
+            end)
+        end
+    )
+
+    return result
+end
