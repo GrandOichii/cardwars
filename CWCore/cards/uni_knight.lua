28dda0dd8791aca192e6f9387271a5536397228c
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-09 04:37:24 -0400

    Implemented Uni-Knight

--- CWCore/cards/uni_knight.lua
+++ CWCore/cards/uni_knight.lua
@@ -1,9 +1,7 @@
--- Status: not implemented - requires "until end of turn" effects implementation
+-- Status: implemented
 
 function _Create(props)
-    error('NOT IMPLEMENTED')
     local result = CardWars:Creature(props)
-    local target = nil
 
     result:AddActivatedEffect({
         -- Pay 1 Action >>> Target Creature in this Lane has -10 ATK.
@@ -19,21 +17,27 @@ function _Create(props)
             local options = {}
             local players = GetPlayers()
             for i = 1, 2 do
-                local player = players[i - 1]
+                local player = players[i]
                 local lane = player.Landscapes[laneI]
+                if lane.Creature ~= nil then
+                    options[#options+1] = lane.Creature.Original.Card.ID
+                end
             end
-            AddActionPoints(playerI, 1)
+
+            local target = TargetCreature(playerI, options, 'Choose a creature to debuff')
+
+            UntilEndOfTurn(function (state)
+                local creature = GetCreatureOrDefault(target)
+                if creature == nil then
+                    return
+                end
+                creature.Attack = creature.Attack - 10
+                if creature.Attack < 0 then
+                    creature.Attack = 0
+                end
+            end)
         end
     })
-
-    result:AddStateModifier(function (state, me)
-        -- TODO
-
-        if target == nil then
-            return
-        end
-        -- GetCreature
-    end)
 
     return result
 end
