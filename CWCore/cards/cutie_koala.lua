0c23b3966e543e5e1771a4682bc530cd46840c0f
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-10 06:59:17 -0400

    Add Cutie Koala

---
+++ CWCore/cards/cutie_koala.lua
@@ -0,0 +1,15 @@
+-- Status: not tested
+
+function _Create(props)
+    local result = CardWars:Creature(props)
+
+    result:AddStateModifier(function (state, me)
+        -- While Cutie Koala has 4 or more Damage on it, it has +2 ATK.
+
+        if me.Original.Damage >= 4 then
+            me.Attack = me.Attack + 2
+        end
+    end)
+
+    return result
+end
