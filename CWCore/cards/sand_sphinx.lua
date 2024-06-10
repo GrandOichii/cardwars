9ac2c4dce31e1ac6d3e3381512122352c32b42c3
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-10 03:07:36 -0400

    Add Sand Sphinx

---
+++ CWCore/cards/sand_sphinx.lua
@@ -0,0 +1,32 @@
+
+-- Status: Implemented
+
+function _Create(props)
+    local result = CardWars:Creature(props)
+
+    local getCreature = function (playerI, laneI)
+        local state = GetState()
+        local player = state.Players[playerI]
+        local landscape = player.Landscapes[laneI]
+        return landscape.Creature
+    end
+
+    result:AddActivatedEffect({
+        -- FLOOP >>> Return a Creature you control in this Lane to its owner's hand.
+
+        checkF = function (me, playerI, laneI)
+            return not me.Original:IsFlooped() and getCreature(playerI, laneI) ~= nil
+        end,
+        costF = function (me, playerI, laneI)
+            FloopCard(me.Original.Card.ID)
+            return true
+        end,
+        effectF = function (me, playerI, laneI)
+            local creature = getCreature(playerI, laneI)
+            ReturnCreatureToOwnersHand(creature.Original.Card.ID)
+
+        end
+    })
+
+    return result
+end
