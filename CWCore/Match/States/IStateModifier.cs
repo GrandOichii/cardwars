ced8834f2c4e89d3d589a993eefc8bac3969838f
Igor Yastrebov <i.hawk@yandex.ru>
2024-06-10 09:07:58 -0400

    Add notes to decks

--- CWCore/Match/States/IStateModifier.cs
+++ CWCore/Match/States/IStateModifier.cs
@@ -1,5 +1,7 @@
 namespace CWCore.Match.States;
 
+// TODO add layering - Puppy Parade has to apply it's effect BEFORE any ATK and DEF boosts
+
 public interface IStateModifier {
     public void Modify(MatchState state);
 }
