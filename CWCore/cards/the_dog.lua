50dafa3fdeea0c31f8a5f4f2514eefbe1492f595
Igor <i.hawk@yandex.ru>
2024-06-26 11:02:03 +0400

    Iplemented The Dog

--- CWCore/cards/the_dog.lua
+++ CWCore/cards/the_dog.lua
@@ -1,31 +1,26 @@
--- Status: not tested
+-- Status: implemented, not sure about the implementation
 
--- TODO doesn't work
 function _Create()
     local result = CardWars:Creature()
 
     Common.ActivatedAbilities.Floop(result,
         'FLOOP >>> Opposing Buildings in this Lane are blank while The Dog is Flooped',
         function (me, playerI, laneI)
-            local opponent = 1 - playerI
-            local buildings = Common.BuildingsInLane(opponent, laneI)
-            local id = me.Original.Card.ID
-            UntilEndOfTurn(function (layer)
-                if layer == CardWars.ModificationLayers.ABILITY_GRANTING_REMOVAL then
-                    local c = GetCreature(id)
-                    if c == nil then
-                        return
-                    end
-                    if not c.Original:IsFlooped() then
-                        return
-                    end
-                    for _, building in ipairs(buildings) do
-                        Common.AbilityGrantingRemoval.RemovaAllFromBuilding(building)
-                    end
-                end
-            end)
         end
     )
+
+    result:AddStateModifier(function (me, layer, zone)
+        if layer == CardWars.ModificationLayers.ABILITY_GRANTING_REMOVAL and zone == CardWars.Zones.IN_PLAY then
+            local opponent = 1 - me.Original.ControllerI
+            local buildings = Common.BuildingsInLane(opponent, me.LaneI)
+            if not me.Original:IsFlooped() then
+                return
+            end
+            for _, building in ipairs(buildings) do
+                Common.AbilityGrantingRemoval.RemovaAllFromBuilding(building)
+            end
+        end
+    end)
 
     return result
 end
